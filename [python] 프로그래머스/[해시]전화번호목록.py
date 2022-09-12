e1b6fe7a920dc74b0d1d7ca3f18aff6106ee8278
Soorim Yoon <68285808+sooo19@users.noreply.github.com>
2022-09-13 02:38:09 +0900

    해시 포켓몬 문제 오류 해결

--- [python] 프로그래머스/[해시]전화번호목록.py
+++ [python] 프로그래머스/[해시]전화번호목록.py
@@ -1,3 +1,6 @@
+# 해시 자료구조 (딕셔너리 사용)
+# https://codingpractices.tistory.com/entry/Python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%95%B4%EC%8B%9C-Hash-%ED%95%B4%EC%8B%B1-Hashing-%EB%AC%B8%EC%A0%9C%EB%A1%9C-%EC%95%8C%EC%95%84%EB%B3%B4%EA%B8%B0
+
 def solution(phone_book):
     answer = True
     hash = {}
@@ -14,7 +17,7 @@ def solution(phone_book):
         for j in i:
             temp += j
             # 해당 숫자의 일부분인 temp와 동일한 숫자가 hash에 있으면, 그 숫자가 접두가사 됨
-            # 단, 
+            # 단, 그 숫자가 자기자신인 경우는 제외 ("119"는 "119"의 접두사가 아님)
             if temp in hash and temp != i:  
                 answer = False
                 
