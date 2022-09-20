2541091e11461ca9c7f26216056d82a643d830e4
Soorim Yoon <68285808+sooo19@users.noreply.github.com>
2022-09-21 02:08:41 +0900

    피보나치 수열(재귀함수 사용)

--- 기초이론예제/Chapter08_다이나믹 프로그래밍/8-1.py
+++ 기초이론예제/Chapter08_다이나믹 프로그래밍/8-1.py
@@ -1,4 +1,11 @@
 # 피보나치 함수 (재귀로 구현)
 
-d = [0]*100
+# 아래 코드의 문제점 : f(n) 함수에 n이 커질수록 수행 시간이 기하급수적으로 늘어남
 
+def fibo(x):
+    if x == 1 or x == 2:        # f(1)과 f(2)는 1이다
+        return 1
+    else:
+        return fibo(x-1) + fibo(x-2)
+
+print(fibo(4))
