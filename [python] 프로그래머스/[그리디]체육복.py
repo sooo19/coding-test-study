60c3c090d2cead7d66248ec921bf7bf54cb72902
Soorim Yoon <68285808+sooo19@users.noreply.github.com>
2022-10-04 23:29:42 +0900

    그리디 체육복 예제 (코드 작성 중)

--- [python] 프로그래머스/[그리디]체육복.py
+++ [python] 프로그래머스/[그리디]체육복.py
@@ -1,3 +1,9 @@
+# 전체 학생의 수 n, 
+# 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 
+# 여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 
+# 체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성하라.
+
 def solution(n, lost, reserve):
     answer = 0
+
     return answer
