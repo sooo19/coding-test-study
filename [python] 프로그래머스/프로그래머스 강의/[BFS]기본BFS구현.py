0a354d3408cd811cbe89c0e62c0612a00d61ba97
Soorim Yoon <68285808+sooo19@users.noreply.github.com>
2022-10-09 15:43:57 +0900

    BFS 기본 코드

---
+++ [python] 프로그래머스/프로그래머스 강의/[BFS]기본BFS구현.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def BFS():
+    queue = deque()
+    queue.append(1)
+    L = 0
+    while(queue):
+        length = len(queue)     # length : 해당 Level에서의 원소의 개수
+        for _ in range(length):
+            v = queue.popleft()
+            print(v, end = ' ')
+            for nv in [v*2, v*2+1]:     # 해당 노드의 아래 가지 2개
+                if nv > 7:
+                    continue    # nv가 7보다 크면 append되지 않음
+                queue.append(nv)
+            L += 1      # Level 증가 (아래로 내려감)
+
+BFS()
