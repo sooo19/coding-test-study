a56d39635ec2284d3ef73617ab425eae37b2f5d7
Soorim Yoon <68285808+sooo19@users.noreply.github.com>
2022-10-15 05:57:06 +0900

    dfs,bfs 단지번호 붙이기 문제

--- 백준/DFS,BFS/2667_단지번호붙이기_정답간결.py
+++ 백준/DFS,BFS/2667_단지번호붙이기_정답간결.py
@@ -1,45 +1,29 @@
 from collections import deque
-# cnt : 영역별 개수
+cnt = 0
 
 def solution():
+    global cnt
     answer = []
     N = int(input())
 
-    # 그래프 입력 코드
-    # graph = []
-    # for i in range(N):
-    #     graph.append(list(map(int, input())))
-
-    # 더 깔끔한 그래프 입력 코드
     graph = [list(map(int, input())) for _ in range(N)]
-
-
-    # 처음엔 아래처럼 코드를 작성해서
-    # 그래프에 숫자가 int형이 아닌 str형으로 저장돼서 아예 0 또는 1로 인식이 안됨
-
-    # for i in range(N):
-    #     s = input()
-    #     s_list = []
-    #     for j in range(N):
-    #         s_list.append(s[j])
-    #     graph.append(s_list)
-    
-
     visited = [[0]*N for _ in range(N)]
 
     for i in range(N):
         for j in range(N):
             if visited[i][j] == 0 and graph[i][j] == 1:
-                print(i, j)
-                cnt = dfs(i, j, graph, visited, N)
+                dfs(i, j, graph, visited, N)
                 answer.append(cnt)
 
     print(len(answer))
+    answer.sort()
     for i in range(len(answer)):
-        print(i)
+        print(answer[i])
+
 
 def dfs(x, y, graph, visited, N):
-    cnt = 0     # 한 영역 당 개수
+    global cnt
+    cnt = 0     # 한 영역 당 단지 개수
 
     queue = deque()
     queue.append((x, y))
@@ -60,9 +44,5 @@ def dfs(x, y, graph, visited, N):
                     queue.append((nx, ny))
                     visited[nx][ny] = 1
                     cnt += 1
-    
-    return cnt
 
-
-# print(solution())
 solution()
