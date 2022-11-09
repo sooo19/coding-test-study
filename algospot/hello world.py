d78191c314d3de71d4cfc3f331d2eb3bb80d7ca6
Soorim Yoon <68285808+sooo19@users.noreply.github.com>
2022-11-10 02:38:55 +0900

    algospot 입출력 연습

---
+++ algospot/hello world.py
@@ -0,0 +1,4 @@
+n = int(input())
+for _ in range(n):
+    s = str(input())
+    print('Hello, ' + s + '!')
---
+++ algospot/출전순서 정하기.py
@@ -0,0 +1,33 @@
+# 문제 링크
+# https://algospot.com/judge/problem/read/MATCHORDER
+
+import sys
+
+# c = int(sys.stdin.readline())
+c = int(input())
+for i in range(c):
+    # n = int(sys.stdin.readline())
+    # team_a = sys.stdin.readline().rstrip().split(" ")
+    # team_b = sys.stdin.readline().rstrip().split(" ")
+
+    n = int(input())
+    team_a = list(map(int, input().split(" ")))
+    team_b = list(map(int, input().split(" ")))
+
+
+    team_a.sort()
+    team_b.sort()
+
+    answer = 0
+    for a in team_a:
+        for b in team_b:
+            if a <= b:
+                answer += 1
+                team_b.remove(b)
+    print(answer)
+
+# n = int(input())
+# for _ in range(n):
+#     s = str(input())
+#     print('Hello, ' + s + '!')
+    
